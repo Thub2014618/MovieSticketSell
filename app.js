1c4cb30a74ed7d363f8cb6295599faaf0d084232
thub2014618 <thub2014618@student.ctu.edu.vn>
2023-10-18 23:48:36 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const contactsRouter = require("./app/routes/contact.route");
 const AdminRouter = require("./app/routes/admin.route");
 const UserRouter = require("./app/routes/user.route");
 
+const ApiError = require("./app/api-error");
+
 app.use(cors());
 app.use(express.json());
 
@@ -15,6 +17,18 @@ app.use("/api/admin", AdminRouter);
 
 app.use("/api/user",UserRouter);
 
+
+
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+    }); 
+});
 
 app.get("/",(req, res)=>{
     res.json({ message: "Welcome to movie sticket sell application."});
